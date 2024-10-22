a4958b7c71e82b96e2e5ded49b02eb00f7688cc4
13581842 <13581842@student.uts.edu.au>
2024-10-22 18:17:05 +1100

    KinovaLink6 Created w/ DH - Parameters
    Created Kinova Link 6.
    - Fixing plyfiles to for the model to work.

---
+++ @KinovaLink6/KinovaLink6.m
@@ -0,0 +1,45 @@
+classdef KinovaLink6 < RobotBaseClass
+    properties(Access = public)              
+        plyFileNameStem = 'KinovaLink6';
+    end
+    
+    methods
+%% Define robot Function 
+    function self = KinovaLink6(baseTr)
+			self.CreateModel();
+            if nargin < 1			
+				baseTr = eye(4);				
+            end
+            self.model.base = self.model.base.T * baseTr * trotz(pi/2);
+            
+            self.PlotAndColourRobot();         
+        end
+
+%% Create the robot model
+        function CreateModel(self)   
+            % Create the Kinova Link 6 Robot
+            link(1) = Link([0,     0.1950,     0.11024,    pi/2]);
+            link(2) = Link([0,  0,         -0.485,      pi]);
+            link(3) = Link([0,  -0.022447,   0,          pi/2]);
+            link(4) = Link([0,     -0.37491,    0,          pi/2]);
+            link(5) = Link([0, -0.139879,   0.086,      pi/2]);
+            link(6) = Link([0,   0.179028,   0,          0]);
+            
+            % Incorporate joint limits
+            link(1).qlim = [-360 360]*pi/180;
+            link(2).qlim = [-90 90]*pi/180;
+            link(3).qlim = [-170 170]*pi/180;
+            link(4).qlim = [-360 360]*pi/180;
+            link(5).qlim = [-360 360]*pi/180;
+            link(6).qlim = [-360 360]*pi/180;
+        
+            link(2).offset = -pi/2;
+            link(3).offset = pi/2;
+            link(4).offset = -pi/2;
+            link(5).offset = pi/2;
+            
+            self.model = SerialLink(link,'name',self.name);
+        end
+     
+    end
+end
