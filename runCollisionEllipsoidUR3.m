b12987b05b9431283b598b453c4277676330ec8c
Johan24445 <Dang.K.Tran@student.uts.edu.au>
2024-10-29 17:13:02 +1100

    Add Ellipsoid Collisions

---
+++ runCollisionEllipsoidUR3.m
@@ -0,0 +1,60 @@
+%% Test collision
+function runCollisionEllipsoidUR3()
+    axis ([-1.5 0.8 -1 1 0 0.8])
+    hold on
+    r = UR3; 
+    
+    centerPoints = [0.0, 0.0, 0.05; % Base 
+                    0.0, -0.01, 0.01; % Link 1 
+                    0.125, 0.0, 0.125; % Link 2 
+                    0.105, 0.0, 0.05; % Link 3 
+                    0.0, 0.0, 0.01; % Link 4 
+                    0.0, 0.0, 0.06; % Link 5 
+                    0.0, 0.0, 0.0;]; % end-effector
+                
+    radii = [0.2, 0.1, 0.15;  
+             0.2, 0.1, 0.15;
+             0.2, 0.1, 0.15; 
+             0.2, 0.1, 0.15; 
+             0.2, 0.1, 0.15;
+             0.2, 0.1, 0.15; 
+             0.0, 0.0, 0.0;]; 
+         
+    
+     
+     collision = CollisionEllipsoid(r, centerPoints, radii);
+     collision.plotEllipsoids();
+     
+    
+     initMeshPosition = [-1.3,0,0.1];
+     mesh = Mesh(0.2,20,initMeshPosition);
+     % mesh.updatePlot();
+     % Movement parameters for the mesh
+     meshMovement = transl(0.08, 0, 0); % Movement vector (e.g., moving in -X direction)
+     steps = 25;  % Number of movement steps
+     
+    
+    for i = 1:steps
+        % Move the mesh incrementally
+        newMeshPosition = transl(initMeshPosition)  * meshMovement^i;
+        newMeshPosition= newMeshPosition(1:3,4)';
+        mesh.move(newMeshPosition);  % Update the mesh position in 3D space
+        mesh.updateParameters(0.2,10,newMeshPosition)
+    
+        % Get the updated mesh points and check for collisions with the robot
+        meshPoints = mesh.getPoints();
+        collisionDetected = collision.checkCollision(meshPoints);
+        
+        % If a collision is detected, display a warning and break the loop
+        if collisionDetected
+            fprintf('Collision detected at step: %d \n', i);
+            break;
+        end
+        
+        pause(0.1); 
+        drawnow;   
+       
+    end
+    
+end 
+
